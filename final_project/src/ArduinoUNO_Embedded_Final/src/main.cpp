83fac07510603096c62cc6d14a7590e9a81e474c
mattnkrueger <mkrue138@gmail.com>
2025-05-02 12:24:30 -0500

    commenting out codeblocks and testing one by one... controller1A & B working. Testing port D now

--- final_project/src/ArduinoUNO_Embedded_Final/src/main.cpp
+++ final_project/src/ArduinoUNO_Embedded_Final/src/main.cpp
@@ -6,31 +6,31 @@
 // ------------------------------------------ HARDWIRED PINS ------------------------------------------ //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 // RPG Definitions 
-const uint8_t RPG2_B = 13;              // PB5      
-const uint8_t RPG2_A = 12;              // PB4
-const uint8_t RPG1_B = 11;              // PB3
-const uint8_t RPG1_A = 10;              // PB2
+// const uint8_t RPG2_B = 13;              // PB5      
+// const uint8_t RPG2_A = 12;              // PB4
+// const uint8_t RPG1_B = 11;              // PB3
+// const uint8_t RPG1_A = 10;              // PB2
 
 // System Buttons
-const uint8_t BTN_UP_ARROW   = 4;       // PD4
-const uint8_t BTN_HOME       = 3;       // PD3
-const uint8_t BTN_DOWN_ARROW = 2;       // PD2
+// const uint8_t BTN_UP_ARROW   = 4;       // PD4
+// const uint8_t BTN_HOME       = 3;       // PD3
+// const uint8_t BTN_DOWN_ARROW = 2;       // PD2
 
 // Controller Buttons
 const uint8_t BTN_CTRL_1A = 9;          // PB1
 const uint8_t BTN_CTRL_1B = 8;          // PB0
-const uint8_t BTN_CTRL_2A = 7;          // PD7
-const uint8_t BTN_CTRL_2B = 6;          // PD6
+// const uint8_t BTN_CTRL_2A = 7;          // PD7
+// const uint8_t BTN_CTRL_2B = 6;          // PD6
 
 // Controller Joysticks (analog)
-const uint8_t JOYSTICK_1X = A0;         // A0
-const uint8_t JOYSTICK_1Y = A1;         // A1
-const uint8_t JOYSTICK_2X = A2;         // A2
-const uint8_t JOYSTICK_2Y = A3;         // A3
+// const uint8_t JOYSTICK_1X = A0;         // A0
+// const uint8_t JOYSTICK_1Y = A1;         // A1
+// const uint8_t JOYSTICK_2X = A2;         // A2
+// const uint8_t JOYSTICK_2Y = A3;         // A3
 
 // Power Button pins 
-const uint8_t BTN_POWER_1 = A4;         // A4
-const uint8_t BTN_POWER_2 = A5;         // A5
+// const uint8_t BTN_POWER_1 = A4;         // A4
+// const uint8_t BTN_POWER_2 = A5;         // A5
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // ------------------------------------------ Global Variables ------------------------------------------ //
@@ -84,14 +84,14 @@ ISR(PCINT0_vect) {
   // boolean values to build a payload for message
   bool isController1BClicked  = false;
   bool isController1AClicked  = false;
-  bool isRPG1Clockwise        = false;
-  bool isRPG1CounterClockwise = false;
-  bool isRPG2Clockwise        = false;
-  bool isRPG2CounterClockwise = false;
+  // bool isRPG1Clockwise        = false;
+  // bool isRPG1CounterClockwise = false;
+  // bool isRPG2Clockwise        = false;
+  // bool isRPG2CounterClockwise = false;
 
   // check controller 1 B
   if (!(pinB & (1 << PB0) && (prevStateB & (1 << PB0)))) {
-    isController1AClicked = true;
+    isController1BClicked = true;
   }
 
   // check controller 1 A
@@ -99,61 +99,61 @@ ISR(PCINT0_vect) {
     isController1AClicked = true;
   }
 
-   // check RPG 1                                                                        here we perform a similar process to our Lab5, combining both A and B.
-   uint8_t currRPG1A = (pinB & (1 << PB2)) ? 1 : 0;                                   // We shift left by one to make space for rpg1b.
-   uint8_t currRPG1B = (pinB & (1 << PB3)) ? 1 : 0;                                   // Then we combine this with the previous & use codes to determine the rotation.
-   uint8_t currRPG1       = (currRPG1A << 1) | currRPG1B;                             // now a 2 bit AB 00, 01, 10, 11 value
-   uint8_t transitionRPG1 = (prevRPG1A << 3) | (prevRPG1B << 2) | (currRPG1);         // now a 4 bit ABAB value, with cases below
+  //  // check RPG 1                                                                        here we perform a similar process to our Lab5, combining both A and B.
+  //  uint8_t currRPG1A = (pinB & (1 << PB2)) ? 1 : 0;                                   // We shift left by one to make space for rpg1b.
+  //  uint8_t currRPG1B = (pinB & (1 << PB3)) ? 1 : 0;                                   // Then we combine this with the previous & use codes to determine the rotation.
+  //  uint8_t currRPG1       = (currRPG1A << 1) | currRPG1B;                             // now a 2 bit AB 00, 01, 10, 11 value
+  //  uint8_t transitionRPG1 = (prevRPG1A << 3) | (prevRPG1B << 2) | (currRPG1);         // now a 4 bit ABAB value, with cases below
  
-   switch (transitionRPG1) {
-     case 0b0001: case 0b0111: case 0b1110: case 0b1000:
-       // Clockwise
-       isRPG1Clockwise = true;
-       break;
-     case 0b0010: case 0b0100: case 0b1101: case 0b1011:
-       // Counter-clockwise
-       isRPG1CounterClockwise = true;
-       break;
-     default:
-       // on detent; ignore
-       break;
-   }
+  //  switch (transitionRPG1) {
+  //    case 0b0001: case 0b0111: case 0b1110: case 0b1000:
+  //      // Clockwise
+  //      isRPG1Clockwise = true;
+  //      break;
+  //    case 0b0010: case 0b0100: case 0b1101: case 0b1011:
+  //      // Counter-clockwise
+  //      isRPG1CounterClockwise = true;
+  //      break;
+  //    default:
+  //      // on detent; ignore
+  //      break;
+  //  }
 
-   prevRPG1A = currRPG1A; 
-   prevRPG1B = currRPG1B;
+  //  prevRPG1A = currRPG1A; 
+  //  prevRPG1B = currRPG1B;
 
-   // check RPG 2                                                                        here we perform a similar process to our Lab5, combining both A and B.
-   uint8_t currRPG2A = (pinB & (1 << PB4)) ? 1 : 0;                                   // We shift left by one to make space for rpg1b.
-   uint8_t currRPG2B = (pinB & (1 << PB5)) ? 1 : 0;                                   // Then we combine this with the previous & use codes to determine the rotation.
-   uint8_t currRPG2       = (currRPG2A << 1) | currRPG2B;                             // now a 2 bit AB 00, 01, 10, 11 value
-   uint8_t transitionRPG2 = (prevRPG2A << 3) | (prevRPG2B << 2) | (currRPG2);         // now a 4 bit ABAB value, with cases below
+  //  // check RPG 2                                                                        here we perform a similar process to our Lab5, combining both A and B.
+  //  uint8_t currRPG2A = (pinB & (1 << PB4)) ? 1 : 0;                                   // We shift left by one to make space for rpg1b.
+  //  uint8_t currRPG2B = (pinB & (1 << PB5)) ? 1 : 0;                                   // Then we combine this with the previous & use codes to determine the rotation.
+  //  uint8_t currRPG2       = (currRPG2A << 1) | currRPG2B;                             // now a 2 bit AB 00, 01, 10, 11 value
+  //  uint8_t transitionRPG2 = (prevRPG2A << 3) | (prevRPG2B << 2) | (currRPG2);         // now a 4 bit ABAB value, with cases below
  
-   switch (transitionRPG2) {
-     case 0b0001: case 0b0111: case 0b1110: case 0b1000:
-       // Clockwise
-       isRPG2Clockwise = true;
-       break;
-     case 0b0010: case 0b0100: case 0b1101: case 0b1011:
-       // Counter-clockwise
-       isRPG2CounterClockwise = true;
-       break;
-     default:
-       // on detent; ignore
-       break;
-   }
+  //  switch (transitionRPG2) {
+  //    case 0b0001: case 0b0111: case 0b1110: case 0b1000:
+  //      // Clockwise
+  //      isRPG2Clockwise = true;
+  //      break;
+  //    case 0b0010: case 0b0100: case 0b1101: case 0b1011:
+  //      // Counter-clockwise
+  //      isRPG2CounterClockwise = true;
+  //      break;
+  //    default:
+  //      // on detent; ignore
+  //      break;
+  //  }
 
-   prevRPG2A = currRPG2A; 
-   prevRPG2B = currRPG2B;
+  //  prevRPG2A = currRPG2A; 
+  //  prevRPG2B = currRPG2B;
 
    // now, we build the flags to send over UART                                 Flags       
    uint8_t flags = 0;                                                         
    flags |= isController1BClicked << 0;                                       // b0: controller 1 'B'
    flags |= isController1AClicked << 1;                                       // b1: controller 1 'A'
-   flags |= isRPG1Clockwise << 2;                                             // b2: RPG1 cw
-   flags |= isRPG1CounterClockwise << 3;                                      // b3: RPG1 ccw (these are mutually exclusive)
-   flags |= isRPG2Clockwise << 4;                                             // b4: RPG2 cw
-   flags |= isRPG2CounterClockwise << 5;                                      // b5: RPG2 ccw (again, these are mutually exclusive)
-   flags |= (0 << 6) | ( 0 << 7);                                             // b6,7: 0
+  //  flags |= isRPG1Clockwise << 2;                                             // b2: RPG1 cw
+  //  flags |= isRPG1CounterClockwise << 3;                                      // b3: RPG1 ccw (these are mutually exclusive)
+  //  flags |= isRPG2Clockwise << 4;                                             // b4: RPG2 cw
+  //  flags |= isRPG2CounterClockwise << 5;                                      // b5: RPG2 ccw (again, these are mutually exclusive)
+  //  flags |= (0 << 6) | ( 0 << 7);                                             // b6,7: 0
 
    // update flags and set dirty
    portB_flags = flags;
@@ -165,71 +165,71 @@ ISR(PCINT0_vect) {
    prevStateB = pinB;
 }
 
-/**
- * @brief Construct a new ISR object for PCINT[16..23] (port d)
- * 
- * PCINT16 (PD0): none (uart rx)
- * PCINT17 (PD1): none (uart tx)
- * PCINT18 (PD2): BTN_DOWN_ARROW
- * PCINT19 (PD3): BTN_HOME
- * PCINT20 (PD4): BTN_UP_ARROW
- * PCINT21 (PD5): none 
- * PCINT22 (PD6): BTN_CTRL_2B
- * PCINT23 (PD7): BTN_CTRL_2A
- * 
- */
-ISR(PCINT2_vect) {
-  uint8_t pinD = PIND;        // all pins to be masked below...
+// /**
+//  * @brief Construct a new ISR object for PCINT[16..23] (port d)
+//  * 
+//  * PCINT16 (PD0): none (uart rx)
+//  * PCINT17 (PD1): none (uart tx)
+//  * PCINT18 (PD2): BTN_DOWN_ARROW
+//  * PCINT19 (PD3): BTN_HOME
+//  * PCINT20 (PD4): BTN_UP_ARROW
+//  * PCINT21 (PD5): none 
+//  * PCINT22 (PD6): BTN_CTRL_2B
+//  * PCINT23 (PD7): BTN_CTRL_2A
+//  * 
+//  */
+// ISR(PCINT2_vect) {
+//   uint8_t pinD = PIND;        // all pins to be masked below...
 
-  // boolean values to build a payload for message
-  bool isDownArrowClicked      = false;
-  bool isHomeClicked           = false;
-  bool isUpArrowClicked        = false;
-  bool isController2BClicked   = false;
-  bool isController2AClicked   = false;
+//   // boolean values to build a payload for message
+//   bool isDownArrowClicked      = false;
+//   bool isHomeClicked           = false;
+//   bool isUpArrowClicked        = false;
+//   bool isController2BClicked   = false;
+//   bool isController2AClicked   = false;
 
-  // check down arrow
-  if (!(pinD & (1 << PD2) && (prevStateD & (1 << PD2)))) {
-    isDownArrowClicked = true;
-  }
+//   // check down arrow
+//   if (!(pinD & (1 << PD2) && (prevStateD & (1 << PD2)))) {
+//     isDownArrowClicked = true;
+//   }
 
-  // check home
-  if (!(pinD & (1 << PD3) && (prevStateD & (1 << PD3)))) {
-    isHomeClicked = true;
-  }
+//   // check home
+//   if (!(pinD & (1 << PD3) && (prevStateD & (1 << PD3)))) {
+//     isHomeClicked = true;
+//   }
 
-  // check down arrow
-  if (!(pinD & (1 << PD4) && (prevStateD & (1 << PD4)))) {
-    isUpArrowClicked = true;
-  }
+//   // check down arrow
+//   if (!(pinD & (1 << PD4) && (prevStateD & (1 << PD4)))) {
+//     isUpArrowClicked = true;
+//   }
 
-  // check controller2 button B
-  if (!(pinD & (1 << PD6) && (prevStateD & (1 << PD6)))) {
-    isController2BClicked = true;
-  }
+//   // check controller2 button B
+//   if (!(pinD & (1 << PD6) && (prevStateD & (1 << PD6)))) {
+//     isController2BClicked = true;
+//   }
 
-  // check controller2 button A
-  if (!(pinD & (1 << PD7) && (prevStateD & (1 << PD7)))) {
-    isController2AClicked = true;
-  }
+//   // check controller2 button A
+//   if (!(pinD & (1 << PD7) && (prevStateD & (1 << PD7)))) {
+//     isController2AClicked = true;
+//   }
 
-  // now, we build the flags to send over UART                                 Flags       
-  uint8_t flags = 0;                                                           // b0..1: 0 
-  flags |= isDownArrowClicked << 2;                                            // b2: down arrow clicked
-  flags |= isHomeClicked << 3;                                                 // b3: home clicked
-  flags |= isUpArrowClicked << 4;                                              // b4: up arrow clicked 
-  flags |= isController2BClicked << 6;                                         // b6: controller 'B' clicked
-  flags |= isController2AClicked << 7;                                         // b7: controller 'A' clicked
+//   // now, we build the flags to send over UART                                 Flags       
+//   uint8_t flags = 0;                                                           // b0..1: 0 
+//   flags |= isDownArrowClicked << 2;                                            // b2: down arrow clicked
+//   flags |= isHomeClicked << 3;                                                 // b3: home clicked
+//   flags |= isUpArrowClicked << 4;                                              // b4: up arrow clicked 
+//   flags |= isController2BClicked << 6;                                         // b6: controller 'B' clicked
+//   flags |= isController2AClicked << 7;                                         // b7: controller 'A' clicked
 
-  // update flags and set dirty
-  portB_flags = flags;
+//   // update flags and set dirty
+//   portB_flags = flags;
 
-  if (flags != 0) {
-    portB_dirty = true;
-  }
+//   if (flags != 0) {
+//     portB_dirty = true;
+//   }
 
-  prevStateD = pinD;
-}
+//   prevStateD = pinD;
+// }
 
 /**
  * @brief enable pin change interrupts
@@ -243,8 +243,8 @@ ISR(PCINT2_vect) {
 void enableButtonInterrupts() {
   PCICR  |= (1 << PCIE0) | (1 << PCIE1) | (1 << PCIE2);  // turn on pcint for b, c, and d ports
   PCMSK0 |= 0b00111111;                                  // PCINT[0..5] (port b)
-  PCMSK1 |= 0x00;                                        // NONE PCINT[8..14]
-  PCMSK2 |= 0xFF;                                        // PCINT[16..23] (port d)
+  // PCMSK1 |= 0x00;                                        // NONE PCINT[8..14]
+  // PCMSK2 |= 0xFF;                                        // All portd PCINT[16..23] (port d)
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -264,22 +264,22 @@ void enableButtonInterrupts() {
 void setup() {
   Serial.begin(115200);
 
-  pinMode(RPG1_A, INPUT);
-  pinMode(RPG1_B, INPUT);
-  pinMode(RPG2_A, INPUT);
-  pinMode(RPG2_B, INPUT);
-  pinMode(BTN_UP_ARROW, INPUT_PULLUP);
-  pinMode(BTN_DOWN_ARROW, INPUT_PULLUP);
+  // pinMode(RPG1_A, INPUT);
+  // pinMode(RPG1_B, INPUT);
+  // pinMode(RPG2_A, INPUT);
+  // pinMode(RPG2_B, INPUT);
+  // pinMode(BTN_UP_ARROW, INPUT_PULLUP);
+  // pinMode(BTN_DOWN_ARROW, INPUT_PULLUP);
   pinMode(BTN_CTRL_1A, INPUT_PULLUP);
   pinMode(BTN_CTRL_1B, INPUT_PULLUP);
-  pinMode(BTN_CTRL_2A, INPUT_PULLUP);
-  pinMode(BTN_CTRL_2B, INPUT_PULLUP);
-  pinMode(BTN_POWER_1, INPUT_PULLUP);
-  pinMode(BTN_POWER_2, INPUT_PULLUP);
-  pinMode(JOYSTICK_1X, INPUT);
-  pinMode(JOYSTICK_1Y, INPUT);
-  pinMode(JOYSTICK_2X, INPUT);
-  pinMode(JOYSTICK_2Y, INPUT);
+  // pinMode(BTN_CTRL_2A, INPUT_PULLUP);
+  // pinMode(BTN_CTRL_2B, INPUT_PULLUP);
+  // pinMode(BTN_POWER_1, INPUT_PULLUP);
+  // pinMode(BTN_POWER_2, INPUT_PULLUP);
+  // pinMode(JOYSTICK_1X, INPUT);
+  // pinMode(JOYSTICK_1Y, INPUT);
+  // pinMode(JOYSTICK_2X, INPUT);
+  // pinMode(JOYSTICK_2Y, INPUT);
 
   enableButtonInterrupts();
   sei();
@@ -307,23 +307,23 @@ void loop() {
     Serial.print("controller1A: ");
     Serial.println((portB_flags & (1 << 1)) != 0 ? "1" : "0");
 
-    Serial.print("RPG1Clockwise: ");
-    Serial.println((portB_flags & (1 << 2)) != 0 ? "1" : "0");
+    // Serial.print("RPG1Clockwise: ");
+    // Serial.println((portB_flags & (1 << 2)) != 0 ? "1" : "0");
 
-    Serial.print("RPG1CounterClockwise: ");
-    Serial.println((portB_flags & (1 << 3)) != 0 ? "1" : "0");
+    // Serial.print("RPG1CounterClockwise: ");
+    // Serial.println((portB_flags & (1 << 3)) != 0 ? "1" : "0");
 
-    Serial.print("RPG2Clockwise: ");
-    Serial.println((portB_flags & (1 << 4)) != 0 ? "1" : "0");
+    // Serial.print("RPG2Clockwise: ");
+    // Serial.println((portB_flags & (1 << 4)) != 0 ? "1" : "0");
 
-    Serial.print("RPG2CounterClockwise: ");
-    Serial.println((portB_flags & (1 << 5)) != 0 ? "1" : "0");
+    // Serial.print("RPG2CounterClockwise: ");
+    // Serial.println((portB_flags & (1 << 5)) != 0 ? "1" : "0");
 
-    Serial.print("Reserved6: ");
-    Serial.println((portB_flags & (1 << 6)) != 0 ? "1" : "0");
+    // Serial.print("Reserved6: ");
+    // Serial.println((portB_flags & (1 << 6)) != 0 ? "1" : "0");
 
-    Serial.print("Reserved7: ");
-    Serial.println((portB_flags & (1 << 7)) != 0 ? "1" : "0");
+    // Serial.print("Reserved7: ");
+    // Serial.println((portB_flags & (1 << 7)) != 0 ? "1" : "0");
   }
 
   if (portD_dirty) {
@@ -352,5 +352,4 @@ void loop() {
     Serial.print("Reserved1: ");
     Serial.println((portD_flags & (1 << 1)) != 0 ? "1" : "0");
   }
-
 }
